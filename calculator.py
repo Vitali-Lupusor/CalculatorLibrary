ae918a0b07e216621e06d8260dbd692f66d8449c
Hollywood <44089845+Vitali-Lupusor@users.noreply.github.com>
2020-12-25 21:58:06 +0000

    Add requirements

--- calculator.py
+++ calculator.py
@@ -4,8 +4,10 @@ Calculator library containing basic math operations.
 
 
 def add(first_term, second_term):
+    """TODO."""
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
+    """TODO."""
     return first_term - second_term
