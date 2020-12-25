4a2df3dded38a302ce6be78d46dced7fc6fca035
Vitali-Lupusor <44089845+Vitali-Lupusor@users.noreply.github.com>
2020-12-25 21:34:52 +0000

    Add unit tests for calculator

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+"""
+Unit tests for the calculator library
+"""
+
+# Import local modules
+import calculator
+
+
+class TestCalculator:
+    """Test `calculator.py` code."""
+
+    def test_addition(self):
+        """TODO."""
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        """TODO."""
+        assert 2 == calculator.subtract(4, 2)
