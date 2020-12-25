6bd9789bdb37576aaf3bb2f0084c2108c4b94ed3
Hollywood <44089845+Vitali-Lupusor@users.noreply.github.com>
2020-12-25 23:00:13 +0000

    Add "multiply" function

--- calculator.py
+++ calculator.py
@@ -2,12 +2,22 @@
 Calculator library containing basic math operations.
 """
 
+# Import standard modules
+from typing import TypeVar
 
-def add(first_term, second_term):
+Numerical = TypeVar('Numerical', int, float)
+
+
+def add(first_term: Numerical, second_term: Numerical) -> Numerical:
     """TODO."""
     return first_term + second_term
 
 
-def subtract(first_term, second_term):
+def subtract(first_term: Numerical, second_term: Numerical) -> Numerical:
     """TODO."""
     return first_term - second_term
+
+
+def multiply(first_term: Numerical, second_term: Numerical) -> Numerical:
+    """TODO."""
+    return first_term * second_term
